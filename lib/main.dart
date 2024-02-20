1dca2b34f8214844ea1ac2bb1e558f914d2a00b3
Iago Engel Serafim <iagoengel@yahoo.com>
2024-02-20 19:13:32 -0300

    deleting unused code

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:flutter_pomodoro/store/pomodoro.store.dart';
 import 'package:provider/provider.dart';
 import 'package:flutter_pomodoro/pages/pomodoro.dart';
-import 'package:flutter_pomodoro/store/contador.store.dart';
 
 void main() {
   runApp(const MyApp());
@@ -27,43 +25,6 @@ class MyApp extends StatelessWidget {
           useMaterial3: true,
         ),
         home: const Pomodoro(),
-      ),
-    );
-  }
-}
-
-final store = ContadorStore();
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: const Text('Contador'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Observer(
-              builder: (_) => Text(
-                '${store.contador}',
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: store.increment,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
